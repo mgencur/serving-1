knative/serving

Allow for running conformance tests by users with reduced privileges (#11609)
* Allow for running conformance tests by users with reduced privileges

* Allow passing specific test namespaces via flags

* Overlay for deleting test namespaces from templates

* Readme - running Conformance tests as project admin

* Simplify readme

* Simplify running ytt

* Split resources.yaml

*

test/e2e/autotls/auto_tls_test.go
@@ -58,7 +58,7 @@ func TestTLS(t *testing.T) {
 }
 
 func TestTLSDisabledWithAnnotation(t *testing.T) {
-	clients := test.Setup(t, test.TLSNamespace)
+	clients := test.Setup(t, test.ServingFlags.TLSTestNamespace)
 
 	names := test.ResourceNames{
 		Service: test.ObjectNameForTest(t),
@@ -84,7 +84,7 @@ func TestTLSDisabledWithAnnotation(t *testing.T) {
 }
 
 func testAutoTLS(t *testing.T) {
-	clients := test.Setup(t, test.TLSNamespace)
+	clients := test.Setup(t, test.ServingFlags.TLSTestNamespace)
 
 	names := test.ResourceNames{
 		Service: test.ObjectNameForTest(t),

test/e2e/autotls/config/disablenscert/main.go
@@ -46,7 +46,7 @@ func main() {
 		log.Fatal("Couldn't get REST config", "error", err)
 	}
 
-	clients, err := test.NewClients(cfg, test.ServingNamespace)
+	clients, err := test.NewClients(cfg, test.ServingFlags.TestNamespace)
 	if err != nil {
 		log.Fatal("Failed to create clients: ", err)
 	}

mgencur/serving-1

test/e2e/autotls/auto_tls_test.go
@@ -61,7 +61,7 @@ func TestTLS(t *testing.T) {
 }
 
 func TestTLSDisabledWithAnnotation(t *testing.T) {
-	clients := e2e.SetupWithNamespace(t, test.TLSNamespace)
+	clients := e2e.SetupWithNamespace(t, test.ServingFlags.TLSTestNamespace)
 
 	names := test.ResourceNames{
 		Service: test.ObjectNameForTest(t),
@@ -87,7 +87,7 @@ func TestTLSDisabledWithAnnotation(t *testing.T) {
 }
 
 func testAutoTLS(t *testing.T) {
-	clients := e2e.SetupWithNamespace(t, test.TLSNamespace)
+	clients := e2e.SetupWithNamespace(t, test.ServingFlags.TLSTestNamespace)
 
 	names := test.ResourceNames{
 		Service: test.ObjectNameForTest(t),

test/e2e/autotls/config/disablenscert/main.go
@@ -45,7 +45,7 @@ func main() {
 	if err != nil {
 		log.Fatal("Failed to build config: ", err)
 	}
-	clients, err := test.NewClientsFromConfig(cfg, test.ServingNamespace)
+	clients, err := test.NewClientsFromConfig(cfg, test.ServingFlags.TestNamespace)
 	if err != nil {
 		log.Fatal("Failed to create clients: ", err)
 	}